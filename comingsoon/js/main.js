07dc399e96c5932066e42c1b35cf7c6a9829e046
mhamza2557 <mhamza2551@gmail.com>
2019-04-04 20:57:58 +0500

    Commimg Soon

---
+++ comingsoon/js/main.js
@@ -0,0 +1,25 @@
+(function($) {
+    /*==================================================================
+    [ Simple slide100 ]*/
+
+    $('.simpleslide100').each(function() {
+        var delay = 7000;
+        var speed = 1000;
+        var itemSlide = $(this).find('.simpleslide100-item');
+        var nowSlide = 0;
+
+        $(itemSlide).hide();
+        $(itemSlide[nowSlide]).show();
+        nowSlide++;
+        if (nowSlide >= itemSlide.length) { nowSlide = 0; }
+
+        setInterval(function() {
+            $(itemSlide).fadeOut(speed);
+            $(itemSlide[nowSlide]).fadeIn(speed);
+            nowSlide++;
+            if (nowSlide >= itemSlide.length) { nowSlide = 0; }
+        }, delay);
+    });
+
+
+})(jQuery);
